147369a272da62d02f577a5045cc2824302d3d64
oudream <oudream@126.com>
2020-02-28 11:27:10 +0800

    var

---
+++ man/var/var.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+open https://blog.csdn.net/felix_yujing/article/details/76792933
+
+# ansible使用变量的优先级顺序从高到低为：
+# host_vars下定义变量
+# inventory中单个主机定义变量
+# group_vars下定义变量
+# inventory中组定义变量
+
+ansible-playbook test.yml -e "keyvalue=inputed"
+
+
+cat > debug.yml <<EOF
+- hosts: all
+  tasks:
+    - name: get user home directory
+      shell: >
+             getent passwd {{ user }}  | awk -F: '{ print $6 }'
+      changed_when: false
+      register: user_home
+
+    - name: debug output
+      debug:
+        var: user_home.stdout
+EOF
+ansible-playbook -i hosts debug.yml -e "user=user1"
+
